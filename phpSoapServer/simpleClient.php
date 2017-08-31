d4091f200e45d4b5fb12fcb210259d38963c8f61
C.A. Little <calittle@users.noreply.github.com>
2017-08-31 17:31:38 -0400

    Update simpleClient.php

--- phpSoapServer/simpleClient.php
+++ phpSoapServer/simpleClient.php
@@ -1,9 +1,22 @@
 <?php
+
+#
+# This is an example of how you might create a SOAP service client to call Documaker Web Service.
+# The actual web service provided in this example is non-functional as it only returns a static response
+# however the important part is that you can see how you might take some user-entered data and create base-64 encoded XML
+# which is then placed into a SOAP request and sent to the dummy web service. 
+# Of minor importance is how we parse the response from the dummy web service and grab the necessary elements to invoke 
+# Documaker Interactive.
+#
+
 function createXml($parm1, $parm2, $parm3){
 	# receive input variables. Place into Extract schema.
+	# NOTE WELL::::::::::
+	# This is the most important part of the integration layer: the XML layout here is what 
+	# your Documaker implementation must be configured to accept.
 	$extractXml = base64_encode("<xml><parm1>$parm1</parm1><parm2>$parm2</parm2><parm3>$parm3</parm3>");
 
-	#create soap request
+	#create soap request and insert base64 Encoded XML.
 	return "<soapenv:Header/><soapenv:Body><pub:DoPublishFromImportRequest><pub:DoPublishFromImportRequestV1><com:timeoutMillis>9000000</com:timeoutMillis><v1:JobRequest><req:Payload><req:Transaction><req:Data><com1:Content><com1:Binary>$extractXml</com1:Binary></com1:Content></req:Data></req:Transaction></req:Payload></v1:JobRequest><v1:ResponseProperties><com1:ResponseType>Attachments</com1:ResponseType></v1:ResponseProperties></pub:DoPublishFromImportRequestV1></pub:DoPublishFromImportRequest></soapenv:Body></soapenv:Envelope>";
  
 }
